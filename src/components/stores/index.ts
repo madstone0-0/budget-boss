29778f3a2fa1bd3a5c59d3d8fe7f95b9e5c49154
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-09-16 17:46:10 +0000

    feat(state): Added immer to handle state updating

--- src/components/stores/index.ts
+++ src/components/stores/index.ts
@@ -1,9 +1,13 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 import { User } from "../types";
 
 type State = {
     user: User;
+};
+
+type Actions = {
     updateEmail: (email: string) => void;
     updateId: (id: string) => void;
     clearEmail: () => void;
@@ -11,54 +15,41 @@ type State = {
     setAuth: (value: boolean) => void;
 };
 
-const useStore = create<State>()(
-    devtools(
-        persist(
-            (set) => ({
-                user: {
-                    email: "",
-                    id: null,
-                    isAuthed: false,
+const useStore = create<State & Actions>()(
+    immer(
+        devtools(
+            persist(
+                (set) => ({
+                    user: {
+                        email: "",
+                        id: null,
+                        isAuthed: false,
+                    },
+                    updateEmail: (email) =>
+                        set((state) => {
+                            state.user.email = email;
+                        }),
+                    updateId: (id) =>
+                        set((state) => {
+                            state.user.id = id;
+                        }),
+                    clearEmail: () =>
+                        set((state) => {
+                            state.user.email = "";
+                        }),
+                    clearId: () =>
+                        set((state) => {
+                            state.user.id = null;
+                        }),
+                    setAuth: (value) =>
+                        set((state) => {
+                            state.user.isAuthed = value;
+                        }),
+                }),
+                {
+                    name: "global-storage",
                 },
-                updateEmail: (email) =>
-                    set((state) => ({
-                        user: {
-                            ...state.user,
-                            email: email,
-                        },
-                    })),
-                updateId: (id) =>
-                    set((state) => ({
-                        user: {
-                            ...state.user,
-                            id: id,
-                        },
-                    })),
-                clearEmail: () =>
-                    set((state) => ({
-                        user: {
-                            ...state.user,
-                            email: "",
-                        },
-                    })),
-                clearId: () =>
-                    set((state) => ({
-                        user: {
-                            ...state.user,
-                            id: null,
-                        },
-                    })),
-                setAuth: (value) =>
-                    set((state) => ({
-                        user: {
-                            ...state.user,
-                            isAuthed: value,
-                        },
-                    })),
-            }),
-            {
-                name: "global-storage",
-            },
+            ),
         ),
     ),
 );
